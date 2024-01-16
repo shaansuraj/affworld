896aaa7b9652ff48f882225f4a970d066c66b474
shaansuraj <surajsahu658@gmail.com>
2024-01-16 07:50:28 +0530

    Corrected errors and adjusted components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from '../src/components/auth/login';
+import Signup from '../src/components/auth/signup';
+import PasswordRecovery from '../src/components/auth/passwordRecovery';
+import SecretMessageForm from '../src/components/message/secretMessageForm';
+import SecretMessageBoard from '../src/components/message/secretMessageBoard';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/recover-password" element={<PasswordRecovery />} />
+          <Route path="/post-secret" element={<SecretMessageForm />} />
+          <Route path="/secrets" element={<SecretMessageBoard />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
